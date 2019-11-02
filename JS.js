59e334445b84d579684b7d28ac24a02141ab3911
Elliot <Kochelliot2012@gmail.com>
2019-11-02 17:55:13 +0100

    Implement search feature (and change the world!)

--- JS.js
+++ JS.js
@@ -1,49 +1,46 @@
-console.log("Hello Pecheur");
+const languages = [
+{name:"Ruby", ease:"1", description:"description of Ruby " },
+{name:"C#", ease:"2", description:"description of C#" },
+{name:"Rust", ease:"3", description:"description of Rust" },
+{name:"JavaScript", ease:"4", description:"description of JavaScript" },
+{name:"Ada", ease:"5", description:"description of Ada" },
+{name:"Python", ease:"6", description:"description of Python" },
+{name:"C", ease:"2", description:"description of C " },
+{name:"Java", ease:"7", description:"description of Java" },
+{name:"Lua", ease:"8", description:"description of Lua" },
+{name:"Swift", ease:"9", description:"description of Swift" },
+{name:"Go", ease:"10", description:"description of Go" },
+{name:"Elixir", ease:"1", description:"description of Elixir" }
+];
 
+console.log(languages);
+function renderCard(language) {
+const card = `
+ <div class="card ">
+      <div class="card-body">
+        <h5 class="card-title">${language.name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${language.ease}</h6>
+        <p class="card-text">${language.description}</p>
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+`;
+const div = document.getElementById("search-result")
+div.innerHTML += card;
+}
 
+function renderAllCard() {
+languages.forEach(renderCard)
+}
 
+function onInputChange(event) {
+const input = event.target.value;
+const result = languages.filter(language => containsInput(language,input))
+const div = document.getElementById("search-result")
+div.innerHTML = "";
+result.forEach(renderCard)
+}
 
-function SearchFun() {
-    // Declare variables
-    var input, filter, infoSearch, paragraph, a, i, txtValue;
-    input = document.getElementById('SearchBar');
-    filter = input.value;
-    console.log(filter)
-    infoSearch = document.getElementById("AccesSearch");
-    paragraph = infoSearch.getElementsByTagName('p');
-  
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < paragraph.length; i++) {
-      txtValue  = paragraph[i].getElementsByTagName("p")[0];
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        paragraph[i].style.display = "";
-      } else {
-        paragraph[i].style.display = "none";
-      }
-    }
-  }
-
-
-
-/*const SearchFun=() => {
-    let input=document.getElementById("SearchBar").value.toUpperCase();
-    console.log(input);
-    let infoSearch = document.getElementById("AccesSearch");
-    console.log(infoSearch);
-    console.log(infoSearch.textContent);
-    let paragraph = document.getElementsByTagName("p");
-    console.log(paragraph);
-      for(var i = 0; i<paragraph.length; i++){
-          let textvalue = paragraph.textContent || paragraph.innerHTML;
-          console.log(textvalue)
-          if (input=textvalue.toUpperCase().indexOf(filter)>-1){
-              infoSearch[i].classList.add('filtered');
-          }else{
-             infoSearch[i].classList.add('notfiltered');
-         }
-      }
-    }*/
-
-
-
-
+function containsInput(language,input) {
+return language.description.toLowerCase().indexOf(input.toLowerCase()) >= 0
+}
