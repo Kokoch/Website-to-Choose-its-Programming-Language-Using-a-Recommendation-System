46d3581c3008b2b493bec2d52ff54c6f2c17ae61
Elliot <Kochelliot2012@gmail.com>
2019-11-09 16:48:33 +0100

    test new version

--- JS.js
+++ JS.js
@@ -1,16 +1,36 @@
 const languages = [
-{name:"Ruby", ease:"1", description:"description of Ruby ", bg:"red" },
-{name:"C#", ease:"2", description:"description of C#", bg:"green" },
-{name:"Rust", ease:"3", description:"description of Rust", bg:"gray" },
-{name:"JavaScript", ease:"4", description:"description of JavaScript", bg:"yellow" },
-{name:"Ada", ease:"5", description:"Ada was created in 1980 by Jean Ichbiah under contract to the United States Department of Defense ; structured, statically typed, imperative, and object-oriented high-level programming language; features of ADA include: strong typing, run-time checking, parallel processing and exception handling", bg:"purple" },
-{name:"Python", ease:"6", description:"Python was created by Guido van Rossum and first released in 1991 ; interpreted (executed direcly, doesn’t need compiling), high-level (closer to natural language), general-purpose language ; very popular, has the reputation of being easy to read and to learn", bg:"yellow" },
-{name:"C", ease:"2", description:"C was created by Dennis Ritchie and first appeared in 1972 ; general-purpose, object-oriented language ; cross-platform ; one of the most widely used languages, influenced a lot of well-known languages (C++, Java, Javascript, Python…)", bg:"blue" },
-{name:"Java", ease:"7", description:"Java was created by James Gosling and released in 1996 ; general-purpose, object-oriented language ; can run on all platforms that support Java without the need for recompilation (= very portable), extremely popular (particularly for client-server web applications)", bg:"red" },
-{name:"Lua", ease:"8", description:"description of Lua", bg:"blue" },
-{name:"Swift", ease:"9", description:"description of Swift", bg:"orange" },
-{name:"Go", ease:"10", description:"description of Go", bg:"lblue" },
-{name:"Elixir", ease:"1", description:"description of Elixir", bg:"purple" }
+    {name: "Ruby", ease: "1", description: "description of Ruby ", bg: "red"},
+    {name: "C#", ease: "2", description: "description of C#", bg: "green"},
+    {name: "Rust", ease: "3", description: "description of Rust", bg: "gray"},
+    {name: "JavaScript", ease: "4", description: "description of JavaScript", bg: "yellow"},
+    {
+        name: "Ada",
+        ease: "5",
+        description: "Ada was created in 1980 by Jean Ichbiah under contract to the United States Department of Defense ; structured, statically typed, imperative, and object-oriented high-level programming language; features of ADA include: strong typing, run-time checking, parallel processing and exception handling",
+        bg: "purple"
+    },
+    {
+        name: "Python",
+        ease: "6",
+        description: "Python was created by Guido van Rossum and first released in 1991 ; interpreted (executed direcly, doesn’t need compiling), high-level (closer to natural language), general-purpose language ; very popular, has the reputation of being easy to read and to learn",
+        bg: "yellow"
+    },
+    {
+        name: "C",
+        ease: "2",
+        description: "C was created by Dennis Ritchie and first appeared in 1972 ; general-purpose, object-oriented language ; cross-platform ; one of the most widely used languages, influenced a lot of well-known languages (C++, Java, Javascript, Python…)",
+        bg: "blue"
+    },
+    {
+        name: "Java",
+        ease: "7",
+        description: "Java was created by James Gosling and released in 1996 ; general-purpose, object-oriented language ; can run on all platforms that support Java without the need for recompilation (= very portable), extremely popular (particularly for client-server web applications)",
+        bg: "red"
+    },
+    {name: "Lua", ease: "8", description: "description of Lua", bg: "blue"},
+    {name: "Swift", ease: "9", description: "description of Swift", bg: "orange"},
+    {name: "Go", ease: "10", description: "description of Go", bg: "lblue"},
+    {name: "Elixir", ease: "1", description: "description of Elixir", bg: "purple"}
 ];
 
 
@@ -25,7 +45,7 @@ function renderCard(language) {
           </div>
         </div>
     `;
-    const div = document.getElementById("search-result")
+    const div = document.getElementById("search-result");
     div.innerHTML += card;
 }
 
@@ -35,22 +55,21 @@ function renderAllCard() {
 
 function onInputChange(event) {
     const input = event.target.value.trim();
-     if (input.length==0){
-           renderAllCard()
-        }
-    else {
-    const result = languages.filter(language => getLanguageScore(language,input)>0)
-    result.sort((language1, language2) => getLanguageScore(language2,input)-getLanguageScore(language1,input))
-    const div = document.getElementById("search-result")
-    console.log(result)
-    div.innerHTML = "";
-    result.forEach(renderCard)
+    if (input.length === 0) {
+        renderAllCard()
+    } else {
+        const result = languages.filter(language => getLanguageScore(language, input) > 0);
+        result.sort((language1, language2) => getLanguageScore(language2, input) - getLanguageScore(language1, input));
+        const div = document.getElementById("search-result");
+        console.log(result);
+        div.innerHTML = "";
+        result.forEach(renderCard)
     }
 }
 
-function getLanguageScore(language,input) {
-    const words = input.split(' ').filter(word => word.length > 0)
+function getLanguageScore(language, input) {
+    const words = input.split(' ').filter(word => word.length > 0);
     const languageText = language.description.toLowerCase() + language.name.toLowerCase();
     const wordsIncluded = words.map(word => languageText.includes((word.toLowerCase())));
-    return wordsIncluded.filter( included => included == true).length
+    return wordsIncluded.filter(included => included === true).length
 }
