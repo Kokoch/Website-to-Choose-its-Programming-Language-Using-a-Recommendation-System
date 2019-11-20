c20bc615d9d64b344fddc8f0bbd3db5a0d44515f
Elliot <Kochelliot2012@gmail.com>
2019-11-20 21:57:49 +0100

    Ajout the 4 description Java elixir swift C et modifier quelques police dans les cartes

--- JS.js
+++ JS.js
@@ -1,36 +1,47 @@
 const languages = [
     {
         name: "Ada",
-        ease: 5,
+        ease: 5.5,
         description: "Ada was created in 1980 by Jean Ichbiah under contract to the United States Department of Defense ; structured, statically typed, imperative, and object-oriented high-level programming language; features of ADA include: strong typing, run-time checking, parallel processing and exception handling",
         background: "purple"
     },
     {
         name: "C",
-        ease: 2,
-        description: "C was created by Dennis Ritchie and first appeared in 1972 ; general-purpose, object-oriented language ; cross-platform ; one of the most widely used languages, influenced a lot of well-known languages (C++, Java, Javascript, Python…)",
+        ease: 5,
+        description: "The C language is a structure oriented programming language developed by Dennis Ritchie. The C programming language is used for developing system applications that forms a major portion of operating systems such as Windows, UNIX and Linux.\n" +
+            "Therefore, program that is written in C and that respects certain limitations can be compiled for many different platforms. C is a very portable programming language.\\n" +
+            "It is a procedural language, which means that people can write their programs as a series of step-by-step instructions.\n" +
+            "The language itself has very few keywords, and most things are done using libraries, which are collections of code for them to be reused. To conclude C is a general-purpose and object-oriented programming language.",
         background: "blue"
     },
-    {name: "C#", ease: 2, description: "description of C#", background: "green"},
-    {name: "Elixir", ease: 1, description: "description of Elixir", background: "purple"},
+    {name: "C#", ease: 5.5, description: "description of C#", background: "green"},
+    {name: "Elixir", ease: 1, description: "Plataformatec, a software consultancy, created Elixir in 2012 as a Research and Development project led by José Valim. Elixir’s goal is to be a productive language for writing maintainable and reliable software.\n" +
+            "Elixir runs on top of the Erlang Virtual Machine, which provides a scalable and fault-tolerant foundation. To cope with failures, Elixir provides supervisors which describe how to restart parts of your system when things go awry.\n" +
+            "Elixir use functional programming to promotes a coding style that helps developers write code that is short, concise.\n" +
+            "Elixir ships with a great set of tools to ease development. Mix is a build tool that allows you to easily create projects, manage tasks, run tests...", background: "purple"},
     {name: "Go", ease: 10, description: "description of Go", background: "lblue"},
     {
         name: "Java",
-        ease: 7,
-        description: "Java was created by James Gosling and released in 1996 ; general-purpose, object-oriented language ; can run on all platforms that support Java without the need for recompilation (= very portable), extremely popular (particularly for client-server web applications)",
+        ease: 5,
+        description: "Java Programming Language was written by James Gosling, initially it was named oak Programming Language.\n" +
+            "A Java program when compiled produce byte codes. These byte codes can be transferred via network and can be executed by any Java virtual Machine on Windows, Linux, Mac or any other JVM and get same result every time. ‘Write once, Run Anywhere'. \n" +
+            "Java is programming language the syntax of which is human readable.\n" +
+            "Java is called General Purpose programming language because its capabilities are not limited to any specific application domain rather it can be used in various application domain.\n" +
+            "Java is a class and object oriented programming language which means Java supports inheritance feature of object-oriented Programming Language. ",
         background: "red"
     },
-    {name: "JavaScript", ease: 4, description: "description of JavaScript chien chat ballon", background: "yellow"},
+    {name: "JavaScript", ease: 5, description: "description of JavaScript chien chat ballon", background: "yellow"},
     {name: "Lua", ease: 8, description: "description of Lua", background: "blue"},
     {
         name: "Python",
-        ease: 6,
+        ease: 7,
         description: "Python was created by Guido van Rossum and first released in 1991 ; interpreted (executed direcly, doesn’t need compiling), high-level (closer to natural language), general-purpose language ; very popular, has the reputation of being easy to read and to learn",
         background: "yellow"
     },
     {name: "Ruby", ease: 1, description: "description of Ruby chien ballon ", background: "red"},
     {name: "Rust", ease: 3, description: "description of Rust chat", background: "gray"},
-    {name: "Swift", ease: 9, description: "description of Swift", background: "orange"},
+    {name: "Swift", ease: 9, description: "Swift was introduced at Apple's 2014 Worldwide Developers Conference and it is an alternative to the Objective-C language .\n" +
+            "Swift is a general-purpose, multi-paradigm, powerful and intuitive programming language for macOS, iOS, watchOS, tvOS and beyond. Writing Swift code is interactive and fun, the syntax is concise yet expressive, and Swift includes modern features developers love. Swift code is safe by design, yet also produces software that runs lightning-fast.", background: "orange"},
 ];
 
 
@@ -39,7 +50,7 @@ function renderCard(language) {
      <div class="card ${language.background}">
           <div class="card-body">
             <h5 class="card-title">${language.name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">ease of learning: ${language.ease}/10</h6>
+            <h5 class="card-subtitle mb-2 text-muted"><u>ease of learning: ${language.ease}/10</u></h5>
             <p class="card-text">${language.description}</p>
             <a href="#" class="card-link ">Card link</a>
           </div>
@@ -49,7 +60,7 @@ function renderCard(language) {
     div.innerHTML += card;
 }
 
-function clearCards(){
+function clearCards() {
     const div = document.getElementById("search-result");
     div.innerHTML = "";
 }
@@ -85,17 +96,31 @@ function changeEaseLevel(event) {
 
 function displayCards() {
     document.getElementById('formPage').style.display = 'none';
+    document.getElementById('aboutPage').style.display = 'none';
+    document.getElementById('sourcesPage').style.display = 'none';
     document.getElementById('cardsPage').style.display = 'contents';
 }
 
 function displayForm() {
     document.getElementById('cardsPage').style.display = 'none';
+    document.getElementById('aboutPage').style.display = 'none';
+    document.getElementById('sourcesPage').style.display = 'none';
     document.getElementById('formPage').style.display = 'contents';
 }
 
+function displayAbout() {
+    document.getElementById('cardsPage').style.display = 'none';
+    document.getElementById('formPage').style.display = 'none';
+    document.getElementById('sourcesPage').style.display = 'none';
+    document.getElementById('aboutPage').style.display = 'contents';
+}
 
-
-
+function displaySources() {
+    document.getElementById('cardsPage').style.display = 'none';
+    document.getElementById('formPage').style.display = 'none';
+    document.getElementById('aboutPage').style.display = 'none';
+    document.getElementById('sourcesPage').style.display = 'contents';
+}
 
 
 
